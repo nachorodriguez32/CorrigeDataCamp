2c734d389e27c121c7ede60af4c0d7fd4e9a4198
nachorodriguez32 <narolo3@alumni.uv.es>
2026-04-17 19:50:38 +0200

    Creo el data frame y extraigo los apellidos

--- correccion.R
+++ correccion.R
@@ -1,5 +1,15 @@
 library(openxlsx)
 library(readxl)
+library(stringr)
 
 ficheros_puntos <- list.files("entregas", pattern = "\\.txt$", full.names = TRUE, recursive = TRUE)
 ficheros_pdfs <- list.files("entregas", pattern = "\\.pdf$", full.names = TRUE, recursive = TRUE)
+
+evalua_df <- data.frame()
+
+for (ruta in ficheros_puntos) {
+  partes <- strsplit(ruta, "/")[[1]]
+  nombre_alumno <- partes[2] 
+}
+  apellido <- strsplit(nombre_alumno, " ")[[1]][1]
+
